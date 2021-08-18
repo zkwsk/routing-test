1c66a4eff4cf0eccd7a6b9dd0bdc23c44aab7b99
arellaTV <jeremiah.arella@gmail.com>
2021-08-17 19:52:26 -0700

    Fix urls

--- pages/[userId]/social/[[...socialId]].js
+++ pages/[userId]/social/[[...socialId]].js
@@ -3,7 +3,7 @@ import React from 'react'
 function Social() {
   return (
     <div>
-      This page should render for both /user-123/social &amp; /user-123/social/social-123
+      This page should render for both /user-123/social and /user-123/social/social-123
     </div>
   )
 }
